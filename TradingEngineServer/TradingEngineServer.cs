0f8d4f3258387ef72d69afa76d01e2ecd62f05f7
Colin <Colin@SUESDESKTOP>
2025-01-19 18:56:16 +0800

    Add project files.

---
+++ TradingEngineServer/TradingEngineServer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TradingEngineServer.Core.Configuration;
+
+namespace TradingEngineServer.Core
+{
+    class TradingEngineServer : BackgroundService, iTradingEngineServer
+    {
+        private readonly ILogger<TradingEngineServer> _logger;
+        private readonly TradingEngineServerConfiguration _tradingEngineServerConfig;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger, IOptions<TradingEngineServerConfiguration> config) 
+        { 
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _tradingEngineServerConfig = config.Value ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        public Task Run(CancellationToken token) => ExecuteAsync(token);
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Do something
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TradingEngineServer/iTradingEngineServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Core
+{
+    public interface iTradingEngineServer
+    {
+        Task Run(CancellationToken token);
+
+    }
+}
